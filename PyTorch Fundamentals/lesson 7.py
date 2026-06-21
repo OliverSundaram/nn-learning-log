d63550466e0ef4d1f1cf1492200df537609179ba
Oliver Sundaram <OliverSundaram2009@gmail.com>
2026-06-21 14:16:10 -0400

    Lesson: Tensor manipulation (E.g. Addition, Subtraction, Division, Multiplication, Matrix Multiplication)

---
+++ PyTorch Fundamentals/lesson 7.py
@@ -0,0 +1,32 @@
+import torch
+
+# Manipulating tensors (tensor operations)
+
+# Addition
+# Subtraction
+# Multiplication (element-wise)
+# Division
+# Matrix multiplication
+
+# Addition
+tensor = torch.tensor([1, 2, 3])
+
+print(tensor + 10)
+print(tensor * 10)
+print(tensor - 10)
+# PyTorch inbuilt functions
+print(torch.mul(tensor, 10))
+print(torch.add(tensor, 5))
+print(torch.div(tensor, 2))
+
+
+# Matrix Multiplication (dot product)
+# torch.matmul
+print(torch.matmul(tensor, tensor)) # prints 14
+
+TENSOR1 = torch.tensor([[1, 2, 3]])
+TENSOR2 = torch.tensor([[3],
+                        [2],
+                        [1]])
+
+print(torch.matmul(TENSOR1, TENSOR2))
