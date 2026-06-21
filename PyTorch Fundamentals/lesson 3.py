c1a95333aa0cfff5221faf897d92d10b4c66d974
Oliver Sundaram <OliverSundaram2009@gmail.com>
2026-06-21 14:10:37 -0400

    Lesson: Practice defining tensors with torch.zeros and torch.ones

---
+++ PyTorch Fundamentals/lesson 3.py
@@ -0,0 +1,22 @@
+import torch
+
+# # creating tensors with zeros and 1's
+# # Tensor with zeros
+# zero = torch.zeros(size=(3, 4))
+#
+# print(zero)
+#
+# # Tensor with 1's
+# ones = torch.ones(size=(2, 3))
+# print(ones)
+# print(ones.dtype)
+
+MATRIX = torch.tensor([[1, 2, 3], [4, 5, 6]])
+
+zeros = torch.zeros(size=(2, 1))
+print(zeros)
+
+print(zeros*MATRIX)
+
+# Reflection
+# YOu can create a tensor of a given shape that is filled with 0's or 1's
