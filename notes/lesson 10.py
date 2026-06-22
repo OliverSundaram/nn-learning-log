0df61b02630f28dd272bcb9d63233856005cd39f
Oliver Sundaram <OliverSundaram2009@gmail.com>
2026-06-21 20:42:52 -0400

    Lesson: Tensor aggregation (e.g. min, max, mean, sum)

---
+++ notes/lesson 10.py
@@ -0,0 +1,32 @@
+import torch
+
+# Finding min, max, mean, sum, etc. (tensor aggregation)
+
+# New tensor
+tensor = torch.arange(0, 101, 10)
+
+# Find the min
+min = tensor.min()
+min = torch.min(tensor)
+
+# Find the max
+max = tensor.max()
+max = torch.max(tensor)
+
+# # Find the mean
+# mean = tensor.mean()
+# mean = torch.mean(tensor)
+
+# Got error after running mean()
+# The mean() takes in a floating point or complex dtype
+
+mean = torch.mean(tensor.type(torch.float32))
+
+# Find the sum
+sum = tensor.sum()
+sum = torch.sum(tensor)
+
+print(min)
+print(max)
+print(mean)
+print(sum)
