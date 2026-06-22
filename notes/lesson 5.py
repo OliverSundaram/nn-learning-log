b691b7af1f07c2906899eb7581f466c8b0260a92
Oliver Sundaram <OliverSundaram2009@gmail.com>
2026-06-22 07:10:33 -0400

    Lesson: Practice using tensors of different data types (e.g., float32 and half)

--- notes/lesson 5.py
+++ notes/lesson 5.py
@@ -1,14 +1,14 @@
 import torch
 
-# float_32_tensor = torch.tensor([3.0, 6.0, 9.0],
-#                                dtype=None, # what datatype is the tensor (e.g. float32)
-#                                device=None) # what device is the tensor on (e.g. cpu, gpu)
-#
-# print(float_32_tensor)
-# print(float_32_tensor.dtype)
-#
-# float_16_tensor = float_32_tensor.type(torch.float16) # Cast float_32_tensor into a float16 tensor
-# print(float_16_tensor.dtype)
+float_32_tensor = torch.tensor([3.0, 6.0, 9.0],
+                               dtype=None, # what datatype is the tensor (e.g. float32)
+                               device=None) # what device is the tensor on (e.g. cpu, gpu)
+
+print(float_32_tensor)
+print(float_32_tensor.dtype)
+
+float_16_tensor = float_32_tensor.type(torch.float16) # Cast float_32_tensor into a float16 tensor
+print(float_16_tensor.dtype)
 
 unsigned_int_8_tensor = torch.tensor(data=[-1, 1, 2])
 
