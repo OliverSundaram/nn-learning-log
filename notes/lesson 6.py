b260d386deeda1a30244f408af15ddecf1097a76
Oliver Sundaram <OliverSundaram2009@gmail.com>
2026-06-22 07:16:07 -0400

    Lesson: Getting attributes of tensors (e.g., dtype, ndim, and shape)

--- notes/lesson 6.py
+++ notes/lesson 6.py
@@ -4,13 +4,13 @@ import torch
 # # Get datatype with .dtype
 # # Get shape with .shape
 # # Get device with .device
-#
-# some_tensor = torch.rand(3, 4)
-# print(some_tensor)
-#
-# print(f"Datatype of tensor: {some_tensor.dtype}")
-# print(f"Shape of tensor: {some_tensor.shape}")
-# print(f"Device of tensor: {some_tensor.device}")
+
+some_tensor = torch.rand(3, 4)
+print(some_tensor)
+
+print(f"Datatype of tensor: {some_tensor.dtype}")
+print(f"Shape of tensor: {some_tensor.shape}")
+print(f"Device of tensor: {some_tensor.device}")
 
 tensor = torch.rand(size=(2, 4), dtype=torch.float16, device="cpu")
 print(tensor)
