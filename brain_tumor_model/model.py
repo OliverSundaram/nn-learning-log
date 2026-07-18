291b6121babf6bd04f90e0c3d3a9a69e0251a639
Oliver Sundaram <OliverSundaram2009@gmail.com>
2026-07-18 13:11:58 -0400

    Move brain tumor classifier files to new repo

--- brain_tumor_model/model.py
+++
@@ -1,8 +0,0 @@
-from torch import nn
-
-from torchvision import models
-
-model = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
-
-num_ftrs = model.fc.in_features
-model.fc = nn.Linear(in_features=num_ftrs, out_features=4)
